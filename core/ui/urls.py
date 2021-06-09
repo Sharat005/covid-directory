4f9b2a15d04ea31c4a2c68f3e0290b2544229c32
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2021-06-09 16:48:07 -0400

    Add requrirements.txt, set default route

--- core/ui/urls.py
+++ core/ui/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('react', views.react, name='react'),
     path('patientinfo', views.patientinfo, name='patientinfo'),
     path('screen', views.screen, name="screen"),
-    path('dashboard', views.dashboard, name="dashboard"),
+    path('', views.dashboard, name="dashboard"),
     path('zip', views.zip_analytics, name="zip"),
 ]
